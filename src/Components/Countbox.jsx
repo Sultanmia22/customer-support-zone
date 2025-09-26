95082b79573c7ff74fb9e9643ce8f9e1ae72a510
Sultan-mia <sultanmia2257@gmail.com>
2025-09-26 23:01:53 +0600

    count box update

--- src/Components/Countbox.jsx
+++ src/Components/Countbox.jsx
@@ -9,7 +9,7 @@ const Countbox = ({clickedData,resolve}) => {
     <div className="max-w-[1440px] mx-auto flex md:flex-row flex-col gap-10 my-[70px]  ">
       <div className=" relative flex-1 flex justify-center items-center  flex-col p-10 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-lg overflow-hidden mx-7 md:mx-0">
        
-        <div className=" flex justify-center md:justify-between absolute top-0  h-auto ">
+        <div className=" flex justify-center md:justify-between absolute top-0 left-0 right-0 h-auto ">
           <img src={bgImg1} alt="vector"  />               
           <img src={bgImg1} alt="vector" className=" rotate-y-180 " />               
         </div>
@@ -20,7 +20,7 @@ const Countbox = ({clickedData,resolve}) => {
 
       <div className=" relative flex-1 flex justify-center items-center  flex-col p-10 bg-gradient-to-r from-[#54CF68] to-[#00827A] text-white rounded-lg mx-7 md:mx-0 ">
 
-        <div className=" flex  justify-center md:justify-between absolute top-0  h-auto ">
+        <div className=" flex  justify-center md:justify-between absolute top-0 left-0 right-0  h-auto ">
           <img src={bgImg1} alt="vector"  />               
           <img src={bgImg1} alt="vector"  className=" rotate-y-180 " />               
         </div>
